6698d808d52ce6ce65fd70fb462c56230c5a992c
mfright <uj020288@yahoo.co.jp>
2014-09-28 01:44:06 +0900

    カスタム版のソースコードを追加

---
+++ EasyLock-Custom/EasyLockCustom/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace EasyLock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerMouseCheck = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "EasyLock";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.lockToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(102, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.exitToolStripMenuItem.Text = "Quit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lockToolStripMenuItem
+            // 
+            this.lockToolStripMenuItem.Name = "lockToolStripMenuItem";
+            this.lockToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.lockToolStripMenuItem.Text = "Lock";
+            this.lockToolStripMenuItem.Click += new System.EventHandler(this.lockToolStripMenuItem_Click);
+            // 
+            // timerMouseCheck
+            // 
+            this.timerMouseCheck.Enabled = true;
+            this.timerMouseCheck.Interval = 30000;
+            this.timerMouseCheck.Tick += new System.EventHandler(this.timerMouseCheck_Tick);
+            // 
+            // keyboardHook1
+            // 
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "EasyLock";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timerMouseCheck;
+        private System.Windows.Forms.ToolStripMenuItem lockToolStripMenuItem;
+    }
+}
+
---
+++ EasyLock-Custom/EasyLockCustom/Form1.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * 
+ * ソースコード参照
+ * http://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q1080106570
+ * http://hongliang.seesaa.net/article/7539988.html
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+namespace EasyLock
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        // kernel32 APIを使用する。
+        [DllImport("kernel32.dll")]
+        extern static ExecutionState SetThreadExecutionState(ExecutionState esFlags);
+
+        //引数のExecutionState列挙体
+        [FlagsAttribute]
+        public enum ExecutionState : uint
+        {
+            // Return value of failed.
+            Null = 0,
+
+            // Anti standby.
+            SystemRequired = 1,
+
+            // Anti Display-off
+            DisplayRequired = 2,
+
+            // continuous
+            Continuous = 0x80000000,
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Visible = false;
+
+
+
+            
+        }
+
+        
+
+
+
+        // Quitを押したとき
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Exit application.
+            Application.Exit();
+        }
+
+        
+
+
+        // 定期的に、画面ロックされないようアクションする
+        private void timerMouseCheck_Tick(object sender, EventArgs e)
+        {
+            
+            // DisplayRequiredをSetThreadExecutionStateへ送信.
+            ExecutionState es = new ExecutionState();
+            es = ExecutionState.DisplayRequired;
+            SetThreadExecutionState(es);
+                
+
+        }
+
+        /// <summary>
+        /// 初期化。Form1は見えないようにする。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+
+            
+        }
+
+        /// <summary>
+        /// 画面ロックする処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            windowLock();
+        }
+
+
+        public void windowLock()
+        {
+            System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+        }
+        
+    }
+}
